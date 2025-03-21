34cb51dac072abe86e323b7dd8b9d2b3b01301e8
juanjmg <juanjosemorales1986@gmail.com>
2025-03-21 18:43:32 -0300

    cambios netlify

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_C5RhaItq.mjs';
+import { manifest } from './manifest_Cw-CZN5M.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -89,7 +89,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_astro-internal_middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "beb77d9f-752b-4c6d-9418-2d7bb9175787"
+    "middlewareSecret": "e7205e95-21d3-47a3-82e6-1c06824e938f"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
