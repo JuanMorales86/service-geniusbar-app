a635cdcf3882ed2e7eaa895af97ab3ce0bd7b293
juanjmg <juanjosemorales1986@gmail.com>
2025-05-08 15:58:16 -0300

    render run build again

--- start.js
+++ start.js
@@ -1,6 +1,10 @@
 const port = process.env.PORT || 4321;
 const host = '0.0.0.0';
 
-import( './dist/server/entry.mjs').then(({start}) => {
-    start({ port, host })
+import('./dist/server/entry.mjs').then((server) => {
+    if(typeof server.default === 'function'){
+        server.default({port, host});
+    } else {
+        console.error('[❌] No se encontró una función exportada en entry.mjs');
+    }
 });
